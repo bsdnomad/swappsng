1492bb99f0247ad759be5326d0eea64bfa58af43
bsdnomad <kliment.ognianov@gmail.com>
2016-10-15 02:17:12 +0300

    Module 01 solution

---
+++ module01-solution/app.js
@@ -0,0 +1,68 @@
+(function(){
+    'use strict';
+
+    // Define application
+    angular.module('Module01App', [])
+        .controller('Module01Controller', Module01Controller)
+    ;
+
+    // Protect DI/s from minification
+    Module01Controller.$inject = ['$scope'];
+
+    // Controller definition
+    function Module01Controller($scope){
+        var _STYLE_ERR      = {color: '#ff0000'};
+        var _STYLE_OK       = {color: '#00b674'};
+
+        var _MSG_TOOMUCH    = 'Too much';
+        var _MSG_ENJOY      = 'Enjoy';
+        var _MSG_ERROR      = 'Please enter data first';
+
+        $scope.message      = '';
+        $scope.msgStyle     = _STYLE_OK;
+
+        /**
+         * Checks if lunch list is empty, has less or more than 3 meals
+         * ALso, trims input
+         */
+        $scope.checkList = function(){
+            // if(typeof($scope.lunchMenu) != 'undefined'){
+            // or
+            if($scope.lunchMenu !== undefined){
+                var meals           = $scope.lunchMenu.split(',');
+                var filteredMeals   = [];
+
+                // Filter lunch list
+                meals.forEach(function(meal){
+                    if(meal && meal.trim().length){
+                        filteredMeals.push(meal);
+                    }
+                });
+
+                if(filteredMeals.length) {
+                    $scope.message  = filteredMeals.length > 3 ? _MSG_TOOMUCH : _MSG_ENJOY;
+                    $scope.msgStyle = _STYLE_OK;
+                }
+                else
+                {
+                    $scope.message  = _MSG_ERROR;
+                    $scope.msgStyle = _STYLE_ERR;
+
+                    $scope.resetLunchMenu();
+                }
+            }
+            else
+            {
+                $scope.message      = _MSG_ERROR;
+                $scope.msgStyle     = _STYLE_ERR;
+
+                $scope.resetLunchMenu();
+            }
+        };
+
+        // Reset input text
+        $scope.resetLunchMenu = function(){
+            $scope.lunchMenu    = null;
+        };
+    }
+})();
