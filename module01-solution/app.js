f2a1447b745660c76ba39581a189fe3ab0bb4853
Kliment Ognyanov <kliment.ognianov@gmail.com>
2016-10-15 02:39:15 +0300

    Set border style regarding result

--- module01-solution/app.js
+++ module01-solution/app.js
@@ -14,12 +14,17 @@
         var _STYLE_ERR      = {color: '#ff0000'};
         var _STYLE_OK       = {color: '#00b674'};
 
+        var _BD_STYLE_ERR   = {border: '2px solid #ff0000'};
+        var _BD_STYLE_OK    = {border: '2px solid #00b674'};
+        var _BD_STYLE_DFLT  = {};
+
         var _MSG_TOOMUCH    = 'Too much';
         var _MSG_ENJOY      = 'Enjoy';
         var _MSG_ERROR      = 'Please enter data first';
 
         $scope.message      = '';
         $scope.msgStyle     = _STYLE_OK;
+        $scope.inputStyle   = _BD_STYLE_DFLT;
 
         /**
          * Checks if lunch list is empty, has less or more than 3 meals
@@ -35,18 +40,21 @@
                 // Filter lunch list
                 meals.forEach(function(meal){
                     if(meal && meal.trim().length){
+                        console.log(meal.trim(), meal.trim().length);
                         filteredMeals.push(meal);
                     }
                 });
 
                 if(filteredMeals.length) {
-                    $scope.message  = filteredMeals.length > 3 ? _MSG_TOOMUCH : _MSG_ENJOY;
-                    $scope.msgStyle = _STYLE_OK;
+                    $scope.message      = filteredMeals.length > 3 ? _MSG_TOOMUCH : _MSG_ENJOY;
+                    $scope.msgStyle     = _STYLE_OK;
+                    $scope.inputStyle   = _BD_STYLE_OK;
                 }
                 else
                 {
-                    $scope.message  = _MSG_ERROR;
-                    $scope.msgStyle = _STYLE_ERR;
+                    $scope.message      = _MSG_ERROR;
+                    $scope.msgStyle     = _STYLE_ERR;
+                    $scope.inputStyle   = _BD_STYLE_ERR;
 
                     $scope.resetLunchMenu();
                 }
@@ -55,12 +63,13 @@
             {
                 $scope.message      = _MSG_ERROR;
                 $scope.msgStyle     = _STYLE_ERR;
+                $scope.inputStyle   = _BD_STYLE_ERR;
 
                 $scope.resetLunchMenu();
             }
         };
 
-        // Reset input text
+        // Reset input text and border style
         $scope.resetLunchMenu = function(){
             $scope.lunchMenu    = null;
         };
